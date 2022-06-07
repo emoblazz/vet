46e53974a43378a34448680ecf28ae2e3bdc4286
Honeylee Magbanua <emoblazz@gmail.com>
2022-06-07 23:33:22 +0800

    footer update

--- dist/includes/footer.php
+++ dist/includes/footer.php
@@ -1,5 +1,5 @@
   <footer class="main-footer">
-    <strong>Copyright &copy; 2022. Emoblazz <a href="">San Isidro National High School</a>.</strong>
+    <strong>Copyright &copy; 2022.  <a href="">Sann Jose Veterinary Clinic</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
       <b>Version</b> 1.0.0
